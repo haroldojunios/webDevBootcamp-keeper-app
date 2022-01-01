d6be5fe74d73ad0873ef077bd25d6af0d254b8e8
Haroldo Júnio <haroldojunios@gmail.com>
2022-01-01 19:16:37 -0300

    Notes using data from `notes.js`

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
   return <div>
     <Header />
-    <Note
-      title="This is the note title"
-      content="This is the note content"
-    />
+    {notes.map(note => (
+      <Note
+        key={note.key}
+        title={note.title}
+        content={note.content}
+      />
+    ))}
     <Footer />
   </div>;
 }
