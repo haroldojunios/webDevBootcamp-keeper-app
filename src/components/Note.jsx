3add9f9942550e937fd23dcda425b0173ad5a009
Haroldo Júnio <haroldojunios@gmail.com>
2022-01-01 13:33:04 -0300

    Implemented note

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
 function Note() {
-  return <div>
-
+  return <div className="note">
+    <h1>This is the note title</h1>
+    <p>This is the note content</p>
   </div>;
 }
 
