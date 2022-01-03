48e43bc8a3c8944e9f1549b3b51b31b56c499e13
Haroldo Júnio <haroldojunios@gmail.com>
2022-01-02 21:16:02 -0300

    Get new files from `keeper-part-3`

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 function Footer() {
   const currYear = new Date().getFullYear();
-  return <footer>
-    <p>Copyright &copy; {currYear}</p>
-  </footer>;
+  return (
+    <footer>
+      <p>Copyright &copy; {currYear}</p>
+    </footer>
+  );
 }
 
 export default Footer;
