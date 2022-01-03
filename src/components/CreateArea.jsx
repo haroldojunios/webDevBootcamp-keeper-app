8682f84a968f9ccd2efd0b0e878f5d9eab768e4a
Haroldo Júnio <haroldojunios@gmail.com>
2022-01-02 21:37:08 -0300

    Save title and cont. of `CreateArea` w/ `useState`

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreateArea() {
+  const [noteData, setNoteData] = useState({
+    title: "",
+    content: ""
+  });
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+        <input name="title" placeholder="Title"
+          onChange={event => {
+            const { value } = event.target;
+            setNoteData(prevNoteData => ({ ...prevNoteData, title: value }))
+          }}
+          value={noteData.title}
+        />
+        <textarea name="content" placeholder="Take a note..." rows="3"
+          onChange={event => {
+            const { value } = event.target;
+            setNoteData(prevNoteData => ({ ...prevNoteData, content: value }))
+          }}
+          value={noteData.content}
+        />
         <button>Add</button>
       </form>
     </div>
